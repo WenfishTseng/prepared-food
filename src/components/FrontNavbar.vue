582f80388488eb7c2d0dae3746e2c73aedbf80a8
WenfishTseng <twy0171@outlook.com>
2022-03-17 09:52:27 +0800

    feat: 新增Navbar元件

---
+++ src/components/FrontNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="logo navbar-brand" href="#"><h1 class="h4 mb-0">加熱煮藝</h1></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#myNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="navbar-nav mb-2 mb-lg-0 justify-content-end w-100">
+          <li class="nav-item me-lg-3">
+            <router-link class="nav-link" to="/products">產品列表</router-link>
+          </li>
+          <li class="nav-item me-lg-3">
+            <router-link class="nav-link" to="/cart">常見問題</router-link>
+          </li>
+          <li class="nav-item me-lg-3">
+            <router-link class="nav-link" to="/cart">購物車</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+  name: 'FrontNavbar'
+}
+</script>
