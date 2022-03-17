74154d86b60bc775b031453de890f92f59d03cab
WenfishTseng <twy0171@outlook.com>
2022-03-17 09:54:20 +0800

    feat: 更新套件載入

--- src/main.js
+++ src/main.js
@@ -11,12 +11,42 @@ import 'vue-loading-overlay/dist/vue-loading.css'
 // fontawesome
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-
+// 匯入 vee-validate 主套件
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+// 匯入 vee-validate 相關規則
+import { required, email, min, max } from '@vee-validate/rules'
+// 匯入多國語系的功能
+import { localize, setLocale } from '@vee-validate/i18n'
+// 匯入繁體中文語系檔案
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+// swiper
+import { Swiper, SwiperSlide } from 'swiper/vue/swiper-vue'
+// Styles must use direct files imports
+import 'swiper/swiper.scss'
+import { date } from './methods/filters'
 import App from './App.vue'
 import router from './router'
 
+// 定義驗證規則
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+defineRule('max', max)
+// 設定 vee-validate 全域規則
+configure({
+  generateMessage: localize({ zh_TW: zhTW })
+})
+// 設定預設語系
+setLocale('zh_TW')
+
 const app = createApp(App).use(router)
 app.use(VueAxios, axios)
+app.config.globalProperties.$filters = { date }
 app.component('headroom', headroom)
 app.component('Loading', Loading)
+app.component('Form', Form)
+app.component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
+app.component('Swiper', Swiper)
+app.component('SwiperSlide', SwiperSlide)
 app.mount('#app')
