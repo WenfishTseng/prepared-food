7ac343ebd0bdf4c502bb02952fda3993674aabf1
WenfishTseng <twy0171@outlook.com>
2022-03-18 17:27:47 +0800

    feat: 加上產品種類滑動動畫、新增收藏功能

--- src/views/Front/ProductsView.vue
+++ src/views/Front/ProductsView.vue
@@ -9,7 +9,7 @@
   </Loading>
   <div class="container my-5 my-lg-6">
     <div class="row row-cols-6 g-0 mb-lg-5">
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
           @click="getProducts"
           class="d-flex align-items-center justify-content-around border-end border-2 border-light px-3"
@@ -18,36 +18,36 @@
           <p class="mb-0 fw-bold">全部</p>
         </div>
       </div>
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
-          @click="chooseCategory('肉類')"
+          @click="renderProductsByCategory('肉類')"
           class="d-flex align-items-center justify-content-around border-end border-2 border-light px-3"
         >
           <img src="./../../assets/images/meat.png" width="50" height="50" />
           <p class="mb-0 fw-bold">肉類</p>
         </div>
       </div>
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
-          @click="chooseCategory('湯品')"
+          @click="renderProductsByCategory('湯品')"
           class="d-flex align-items-center justify-content-around border-end border-2 border-light px-3"
         >
           <img src="./../../assets/images/hot.png" width="50" height="50" />
           <p class="mb-0 fw-bold">湯品</p>
         </div>
       </div>
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
-          @click="chooseCategory('飯類')"
+          @click="renderProductsByCategory('飯類')"
           class="d-flex align-items-center justify-content-around border-end border-2 border-light px-3"
         >
           <img src="./../../assets/images/rice.png" width="50" height="50" />
           <p class="mb-0 fw-bold">飯類</p>
         </div>
       </div>
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
-          @click="chooseCategory('麵類')"
+          @click="renderProductsByCategory('麵類')"
           class="d-flex align-items-center justify-content-around border-end border-2 border-light px-3"
         >
           <img
@@ -58,9 +58,9 @@
           <p class="mb-0 fw-bold">麵類</p>
         </div>
       </div>
-      <div class="col">
+      <div class="category-hover col py-1">
         <div
-          @click="chooseCategory('甜點')"
+          @click="renderProductsByCategory('甜點')"
           class="d-flex align-items-center justify-content-around px-3"
         >
           <img src="./../../assets/images/lasagna.png" width="50" height="50" />
@@ -68,7 +68,7 @@
         </div>
       </div>
     </div>
-    <div class="row justify-content-end mt-3 mb-4">
+    <div class="row justify-content-center my-5">
       <div class="col-md-4">
         <form class="d-flex">
           <input
@@ -76,18 +76,13 @@
             type="search"
             placeholder="今天要吃甚麼"
           />
-          <button class="btn btn-outline-secondary" type="submit">Search</button>
+          <button class="btn btn-outline-secondary" type="submit">
+            Search
+          </button>
         </form>
       </div>
     </div>
-    <div class="row">
-      <div class="col-lg-2">
-        <ul class="list-group text-center list-group-flush">
-          <li class="list-group-item">最新商品</li>
-          <li class="list-group-item">熱銷好評</li>
-          <li class="list-group-item">店長推薦</li>
-        </ul>
-      </div>
+    <div class="row justify-content-center">
       <div class="col-lg-10">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
           <div class="col" v-for="product in products" :key="product.id">
@@ -126,7 +121,20 @@
               <div class="card-footer bg-transparent border-top-0 px-0">
                 <div class="row row-cols-md-2 g-1">
                   <div class="col">
-                    <button type="button" class="btn btn-outline-danger w-100">
+                    <button
+                      type="button"
+                      class="btn btn-danger text-light w-100"
+                      v-if="favoriteItem.includes(product.id)"
+                      @click="toggleFavorite(product.id)"
+                    >
+                      <i class="bi bi-arrow-through-heart-fill me-1"></i>已收藏
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-outline-danger w-100"
+                      v-else
+                      @click="toggleFavorite(product.id)"
+                    >
                       <i class="bi bi-heart me-1"></i>收藏
                     </button>
                   </div>
@@ -136,10 +144,6 @@
                       class="btn btn-primary w-100"
                       @click="addToCart(product.id)"
                     >
-                      <i
-                        class="fas fa-spinner fa-pulse"
-                        v-show="isCartLoadingItem === product.id"
-                      ></i>
                       <i class="bi bi-cart me-1"></i>購買
                     </button>
                   </div>
@@ -158,28 +162,18 @@ export default {
   name: 'Products',
   data () {
     return {
+      isCartLoadingId: '',
       isLoading: false,
-      products: {}
+      products: {},
+      favoriteItem: JSON.parse(localStorage.getItem('favors')) || []
     }
   },
   methods: {
     getProducts () {
-      this.isLoading = true
-      this.$http
-        .get(
-          `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`
-        )
-        .then((response) => {
-          this.products = response.data.products
-          this.isLoading = false
-        })
-        .catch((error) => {
-          this.isLoading = false
-          alert(error.data.message)
-        })
+      this.renderProductsByCategory()
     },
     addToCart (id, qty = 1) {
-      this.isCartLoadingItem = id
+      this.isLoading = true
       this.$http
         .post(
           `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/cart`,
@@ -188,21 +182,22 @@ export default {
         .then((response) => {
           this.cartData = response.data.data
           // emitter.emit('get-cart')
-          // 讀完清空id
-          this.isCartLoadingItem = ''
           alert(response.data.message)
+          this.isLoading = false
         })
         .catch((error) => {
           alert(error)
           console.dir(error)
         })
     },
-    chooseCategory (category) {
+    renderProductsByCategory (category) {
       this.isLoading = true
+      let apiUrl = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`
+      if (category) {
+        apiUrl = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products?category=${category}`
+      }
       this.$http
-        .get(
-          `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products?category=${category}`
-        )
+        .get(apiUrl)
         .then((response) => {
           this.products = response.data.products
           this.isLoading = false
@@ -211,6 +206,23 @@ export default {
           this.isLoading = false
           alert(error.data.message)
         })
+    },
+    toggleFavorite (id) {
+      // console.log(id)
+      const favorIndex = this.favoriteItem.findIndex((item) => item === id)
+      if (favorIndex < 0) {
+        this.favoriteItem.push(id)
+      } else {
+        this.favoriteItem.splice(id, 1)
+      }
+    }
+  },
+  watch: {
+    favoriteItem: {
+      handler () {
+        localStorage.setItem('favors', JSON.stringify(this.favoriteItem))
+      },
+      deep: true
     }
   },
   created () {
